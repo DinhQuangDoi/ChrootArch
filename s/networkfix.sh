076d52d16814cc45158f1e1201480974fd5e5e2e
Đinh Quang Đới <dinhquangdoi99@gmail.com>
2025-10-10 22:20:22 +0700

    Update networkfix.sh

--- s/networkfix.sh
+++ s/networkfix.sh
@@ -3,20 +3,20 @@
 set -euo pipefail
 
 echo -e "\033[1;96m[*]\033[0m Writing /etc/resolv.conf and /etc/hosts..."
-cat > /etc/resolv.conf <<'EOF'
+su -c cat > /etc/resolv.conf <<'EOF'
 # DNS
 nameserver 8.8.8.8
 nameserver 1.1.1.1
 EOF
 
-cat > /etc/hosts <<'EOF'
+su -c cat > /etc/hosts <<'EOF'
 127.0.0.1 localhost
 ::1 localhost
 EOF
 
 echo -e "\033[1;96m[*]\033[0m Writing ARM mirrorlist (HTTPS first)..."
-mkdir -p /etc/pacman.d
-cat > /etc/pacman.d/mirrorlist <<'EOF'
+su -c mkdir -p /etc/pacman.d
+su -c cat > /etc/pacman.d/mirrorlist <<'EOF'
 Server = https://mirror.archlinuxarm.org/$arch/$repo
 Server = https://sg.mirror.archlinuxarm.org/$arch/$repo
 Server = https://us.mirror.archlinuxarm.org/$arch/$repo
